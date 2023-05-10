a56ef2193617422111d0561dfcfb89703ecb5cbb
DrDroiberg <94693070+DrDroiberg@users.noreply.github.com>
2023-05-10 16:44:57 +0200

    Camera control indépendant

---
+++ camera_control.py
@@ -0,0 +1,16 @@
+from reachy_sdk import ReachySDK
+from reachy_sdk.trajectory import goto
+from reachy_sdk.trajectory import InterpolationMode
+import time
+import cv2 as cv
+
+reachy = ReachySDK(host='localhost')
+
+
+def camera_control():
+    while True:
+        img = reachy.right_camera.last_frame
+
+        cv.imshow('Left Eye', img)
+        cv.waitKey(0.5)
+        cv.destroyAllWindows()
