fef482b50f3b0ae316d73472ff8f48d4f91f59da
DrDroiberg <94693070+DrDroiberg@users.noreply.github.com>
2023-05-11 09:39:58 +0200

    Add the capture of image and storage in a folder using opencv

--- camera_control.py
+++ camera_control.py
@@ -3,14 +3,23 @@ from reachy_sdk.trajectory import goto
 from reachy_sdk.trajectory import InterpolationMode
 import time
 import cv2 as cv
+import os
+
+path = 'C:/Users/vince/PycharmProjects/Reachy_Project/camera_images'
 
 reachy = ReachySDK(host='localhost')
 
+img_file = []
+
 
 def camera_control():
-    while True:
+    for i in range(0, 10):
         img = reachy.right_camera.last_frame
 
         cv.imshow('Left Eye', img)
         cv.waitKey(500)
         cv.destroyAllWindows()
+        img_file.append(img)
+
+    # save the image from img_file in a folder
+        cv.imwrite(os.path.join(path, 'img_'+str(i)+'.jpg'), img_file[i])
