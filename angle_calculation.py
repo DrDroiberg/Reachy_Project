14abc6ccac83082d910c89514ad9e58346705770
DrDroiberg <94693070+DrDroiberg@users.noreply.github.com>
2023-05-15 11:30:41 +0200

    minor fixes

--- angle_calculation.py
+++ angle_calculation.py
@@ -15,14 +15,14 @@ angle_elbow = []
 
 ########################################################################################################################
 # Extract the values from the coordinates_file.txt and put them in a 2D list
-left_shoulder_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/left_shoulder_coords.txt', delimiter=',')
-right_shoulder_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/right_shoulder_coords.txt', delimiter=',')
-left_elbow_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/left_elbow_coords.txt', delimiter=',')
-right_elbow_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/right_elbow_coords.txt', delimiter=',')
-left_wrist_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/left_wrist_coords.txt', delimiter=',')
-right_wrist_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/right_wrist_coords.txt', delimiter=',')
-left_hip_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/left_hip_coords.txt', delimiter=',')
-right_hip_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/right_hip_coords.txt', delimiter=',')
+left_shoulder_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/left_shoulder_coords.txt', delimiter=',')
+right_shoulder_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_shoulder_coords.txt', delimiter=',')
+left_elbow_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/left_elbow_coords.txt', delimiter=',')
+right_elbow_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_elbow_coords.txt', delimiter=',')
+left_wrist_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/left_wrist_coords.txt', delimiter=',')
+right_wrist_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_wrist_coords.txt', delimiter=',')
+left_hip_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/left_hip_coords.txt', delimiter=',')
+right_hip_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_hip_coords.txt', delimiter=',')
 
 ########################################################################################################################
 
@@ -43,29 +43,48 @@ def findAngle(x1, y1, x2, y2, x3, y3):
     return round(degree, 2)
 
 
-def angle_left_shoulder():
-    angle_shoulder = findAngle(left_shoulder_coords[0], left_shoulder_coords[1], left_elbow_coords[0], left_elbow_coords[1], left_hip_coords[0], left_hip_coords[1])
-    print(angle_shoulder)
+def angle_left_shoulder(i):
+    angle_shoulder = findAngle(left_shoulder_coords[i][0], left_shoulder_coords[i][1], left_elbow_coords[i][0], left_elbow_coords[i][1], left_hip_coords[i][0], left_hip_coords[i][1])
     return angle_shoulder
 
-def angle_right_shoulder():
-    angle_shoulder = findAngle(right_shoulder_coords[0], right_shoulder_coords[1], right_elbow_coords[0], right_elbow_coords[1], right_hip_coords[0], right_hip_coords[1])
-    print(angle_shoulder)
+def angle_right_shoulder(i):
+    angle_shoulder = findAngle(right_shoulder_coords[i][0], right_shoulder_coords[i][1], right_elbow_coords[i][0], right_elbow_coords[i][1], right_hip_coords[i][0], right_hip_coords[i][1])
     return angle_shoulder
 
-def angle_left_elbow():
-    angle_elbow = findAngle(left_elbow_coords[0], left_elbow_coords[1], left_wrist_coords[0], left_wrist_coords[1], left_shoulder_coords[0], left_shoulder_coords[1])
-    print(angle_elbow)
+def angle_left_elbow(i):
+    angle_elbow = findAngle(left_elbow_coords[i][0], left_elbow_coords[i][1], left_shoulder_coords[i][0], left_shoulder_coords[i][1], left_wrist_coords[i][0], left_wrist_coords[i][1])
     return angle_elbow
 
-def angle_right_elbow():
-    angle_elbow = findAngle(right_elbow_coords[0], right_elbow_coords[1], right_wrist_coords[0], right_wrist_coords[1], right_shoulder_coords[0], right_shoulder_coords[1])
-    print(angle_elbow)
+def angle_right_elbow(i):
+    angle_elbow = findAngle(right_elbow_coords[i][0], right_elbow_coords[i][1], right_shoulder_coords[i][0], right_shoulder_coords[i][1], right_wrist_coords[i][0], right_wrist_coords[i][1])
     return angle_elbow
 
+for i in range(0, 10):
+    angle = angle_left_shoulder(i)
+
+    # save the left shoulder angle in a file
+    with open('C:/Users/vince/PycharmProjects/Reachy_Project/listes/left_shoulder_angle.txt', 'a') as f:
+        f.write(str(angle) + '\n')
 
 
+for i in range(0, 10):
+    angle = angle_right_shoulder(i)
 
+    # save the right shoulder angle in a file
+    with open('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_shoulder_angle.txt', 'a') as f:
+        f.write(str(angle) + '\n')
 
-angle_left_shoulder()
+for i in range(0, 10):
+    angle = angle_left_elbow(i)
+
+    # save the left elbow angle in a file
+    with open('C:/Users/vince/PycharmProjects/Reachy_Project/listes/left_elbow_angle.txt', 'a') as f:
+        f.write(str(angle) + '\n')
+
+for i in range(0, 10):
+    angle = angle_right_elbow(i)
+
+    # save the right elbow angle in a file
+    with open('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_elbow_angle.txt', 'a') as f:
+        f.write(str(angle) + '\n')
 
