c6310640c078e7b6e117c305c5cf94202d1f33c2
DrDroiberg <94693070+DrDroiberg@users.noreply.github.com>
2023-05-22 17:37:15 +0200

    ajout conversion_px_cm

---
+++ conversion_px_cm.py
@@ -0,0 +1,15 @@
+import numpy as np
+from picture_calculation import offset_distance
+
+# Get the coordinates of the right wrist and right elbow
+right_wrist_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_wrist_coords.txt', delimiter=',')
+right_elbow_coords = np.loadtxt('C:/Users/vince/PycharmProjects/Reachy_Project/listes/right_elbow_coords.txt', delimiter=',')
+
+# lenght of the arm of reachy = 250 mm
+
+
+pixel_L = offset_distance(right_elbow_coords[0][0], right_elbow_coords[0][1], right_wrist_coords[0][0], right_wrist_coords[0][1])
+cm_L = pixel_L / 250000
+def coeff_px_to_cm():
+    return cm_L
+
