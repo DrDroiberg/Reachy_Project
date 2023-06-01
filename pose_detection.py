0a9cdca18d799d6d7b46bb4c37e3fede62f9d78e
DrDroiberg <94693070+DrDroiberg@users.noreply.github.com>
2023-06-01 16:17:12 +0200

    add of new data (real and cartesian)

--- pose_detection.py
+++ pose_detection.py
@@ -8,9 +8,9 @@ import mediapipe as mp
 # import PyQt5
 # from PIL import Image
 # from IPython.display import Video
-import nb_helpers
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# import nb_helpers
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import os
 
@@ -24,42 +24,48 @@ mp_face_mesh = mp.solutions.face_mesh
 # Read the image file
 path = 'C:/Users/vince/PycharmProjects/Reachy_Project/recognised_images'
 path_txt = 'C:/Users/vince/PycharmProjects/Reachy_Project/data_list'
-folder  = 'camera_opencv'
-def pose_recognition():
-    for i in range(0, 10):
+folder = 'camera_opencv'
 
-        with mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5, model_complexity=2, enable_segmentation=True) as pose:
-            body_image = cv.imread('C:/Users/vince/PycharmProjects/Reachy_Project/' + folder + '/img_' + str(i) + '.jpg')
+
+def pose_recognition(duration):
+    for i in range(0, duration):
+
+        file = "camera_opencv/img_" + str(i) + ".jpg"
+
+        with mp_pose.Pose(static_image_mode=True,
+                          model_complexity=2,
+                          enable_segmentation=True) as pose:
+
+            body_image = cv.imread(file)
+
             results = pose.process(cv.cvtColor(body_image, cv.COLOR_BGR2RGB))
 
         annotated_image = body_image.copy()
 
-        mp_drawing.draw_landmarks(annotated_image, results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS,
-                                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        mp_drawing.draw_landmarks(annotated_image,
+                                  results.pose_landmarks,
+                                  mp_pose.POSE_CONNECTIONS,
+                                  landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
-        cv.imwrite(os.path.join(path, 'body_recognise_' + str(i) + '.jpg'), body_image)
+        filename = "recognised_images/body_recognise_" + str(i) + ".png"
+        cv.imwrite(filename, annotated_image)
 
-        data = np.empty((3, len(mp_holistic.PoseLandmark)))
+        data_rlworld = np.empty((3, len(mp_holistic.PoseLandmark)))
+        data_xyz = np.empty((3, len(mp_holistic.PoseLandmark)))
 
         landmarks = results.pose_world_landmarks.landmark
         for k in range(len(mp_holistic.PoseLandmark)):
-            data[0, k] = landmarks[k].x
-            data[1, k] = landmarks[k].y
-            data[2, k] = landmarks[k].z
+            data_rlworld[0, k] = landmarks[k].x
+            data_rlworld[1, k] = landmarks[k].y
+            data_rlworld[2, k] = landmarks[k].z
 
-        np.savetxt(os.path.join(path_txt, 'data_img_' + str(i) + '.txt'), data, delimiter=',', fmt='%1.3f') #
+        np.savetxt(os.path.join(path_txt, 'data_rlworld_img_' + str(i) + '.txt'), data_rlworld, delimiter=',',
+                   fmt='%1.3f')  #
 
+        landmarks = results.pose_landmarks.landmark
+        for k in range(len(mp_holistic.PoseLandmark)):
+            data_xyz[0, k] = landmarks[k].x
+            data_xyz[1, k] = landmarks[k].y
+            data_xyz[2, k] = landmarks[k].z
 
-    # fig = plt.figure(figsize=(10, 10)) # inches
-    # ax = fig.add_subplot(111, projection='3d')
-    #
-    # ax.plot(data[2, 16], data[0, 16], data[1, 16], zdir='y')
-    #
-    # ax.set_xlabel('X')
-    # ax.set_ylabel('Y')
-    # ax.set_zlabel('Z')
-    #
-    # plt.show()
-
-
-
+        np.savetxt(os.path.join(path_txt, 'data_xyz_img_' + str(i) + '.txt'), data_xyz, delimiter=',', fmt='%1.3f')  #
